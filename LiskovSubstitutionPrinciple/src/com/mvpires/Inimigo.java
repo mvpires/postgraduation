69d4a463e041ca1a5708fb23ffa7580e3a1ddb94
Marcus Vinícius Pires <mvpires@gmail.com>
2016-06-07 11:52:29 -0300

    Third project
    Liskov

---
+++ LiskovSubstitutionPrinciple/src/com/mvpires/Inimigo.java
@@ -0,0 +1,18 @@
+package com.mvpires;
+
+/**
+ * Created by mv_pi on 07/06/2016.
+ */
+public class Inimigo extends Personagem {
+
+    public Inimigo()
+    {
+        super();
+    }
+
+    @Override
+    public void mover()
+    {
+        System.out.println("Movendo o inimigo");
+    }
+}
---
+++ LiskovSubstitutionPrinciple/src/com/mvpires/Player.java
@@ -0,0 +1,19 @@
+package com.mvpires;
+
+/**
+ * Created by mv_pi on 07/06/2016.
+ */
+public class Player extends Personagem  {
+
+    public Player() {
+        super();
+
+    }
+
+    @Override
+    public void mover()
+    {
+        System.out.println("Movendo o player");
+    }
+
+}
